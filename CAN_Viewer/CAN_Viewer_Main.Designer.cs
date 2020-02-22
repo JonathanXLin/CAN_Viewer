e8f3ae974d144c026a1147959761a903a373b4d6
Jonathan Lin <jlin1999@gmail.com>
2020-02-22 17:07:12 -0500

    initial

---
+++ CAN_Viewer/CAN_Viewer_Main.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace CAN_Viewer
+{
+    partial class CAN_Viewer_Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CAN_Viewer_Main));
+            this.statusStrip_status = new System.Windows.Forms.StatusStrip();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.menuStrip_menu = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem_database = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.optionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem_logfile = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip_menu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip_status
+            // 
+            this.statusStrip_status.Location = new System.Drawing.Point(0, 538);
+            this.statusStrip_status.Name = "statusStrip_status";
+            this.statusStrip_status.Size = new System.Drawing.Size(984, 22);
+            this.statusStrip_status.TabIndex = 0;
+            this.statusStrip_status.Text = "statusStrip1";
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(0, 24);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(161, 514);
+            this.checkedListBox1.TabIndex = 1;
+            // 
+            // menuStrip_menu
+            // 
+            this.menuStrip_menu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.toolsToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip_menu.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip_menu.Name = "menuStrip_menu";
+            this.menuStrip_menu.Size = new System.Drawing.Size(984, 24);
+            this.menuStrip_menu.TabIndex = 2;
+            this.menuStrip_menu.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem_logfile,
+            this.openToolStripMenuItem_database,
+            this.toolStripSeparator2,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // openToolStripMenuItem_database
+            // 
+            this.openToolStripMenuItem_database.Image = ((System.Drawing.Image)(resources.GetObject("openToolStripMenuItem_database.Image")));
+            this.openToolStripMenuItem_database.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.openToolStripMenuItem_database.Name = "openToolStripMenuItem_database";
+            this.openToolStripMenuItem_database.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.D)));
+            this.openToolStripMenuItem_database.Size = new System.Drawing.Size(217, 22);
+            this.openToolStripMenuItem_database.Text = "&Open Database File";
+            this.openToolStripMenuItem_database.Click += new System.EventHandler(this.openToolStripMenuItem_database_Click);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(214, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(217, 22);
+            this.exitToolStripMenuItem.Text = "E&xit";
+            // 
+            // toolsToolStripMenuItem
+            // 
+            this.toolsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.optionsToolStripMenuItem});
+            this.toolsToolStripMenuItem.Name = "toolsToolStripMenuItem";
+            this.toolsToolStripMenuItem.Size = new System.Drawing.Size(47, 20);
+            this.toolsToolStripMenuItem.Text = "&Tools";
+            // 
+            // optionsToolStripMenuItem
+            // 
+            this.optionsToolStripMenuItem.Name = "optionsToolStripMenuItem";
+            this.optionsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.optionsToolStripMenuItem.Text = "&Options";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "&Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
+            this.aboutToolStripMenuItem.Text = "&About...";
+            // 
+            // openToolStripMenuItem_logfile
+            // 
+            this.openToolStripMenuItem_logfile.Image = ((System.Drawing.Image)(resources.GetObject("openToolStripMenuItem_logfile.Image")));
+            this.openToolStripMenuItem_logfile.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.openToolStripMenuItem_logfile.Name = "openToolStripMenuItem_logfile";
+            this.openToolStripMenuItem_logfile.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.L)));
+            this.openToolStripMenuItem_logfile.Size = new System.Drawing.Size(217, 22);
+            this.openToolStripMenuItem_logfile.Text = "&Open CAN Logfile";
+            this.openToolStripMenuItem_logfile.Click += new System.EventHandler(this.openToolStripMenuItem_logfile_Click);
+            // 
+            // CAN_Viewer_Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 560);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.statusStrip_status);
+            this.Controls.Add(this.menuStrip_menu);
+            this.MinimumSize = new System.Drawing.Size(500, 300);
+            this.Name = "CAN_Viewer_Main";
+            this.Text = "CAN Viewer v0.1";
+            this.Load += new System.EventHandler(this.CAN_Viewer_Main_Load);
+            this.menuStrip_menu.ResumeLayout(false);
+            this.menuStrip_menu.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip_status;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.MenuStrip menuStrip_menu;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem_database;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem optionsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem_logfile;
+    }
+}
+
---
+++ CAN_Viewer/CAN_Viewer_Main.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Security;
+using System.Diagnostics;
+
+namespace CAN_Viewer
+{
+    public partial class CAN_Viewer_Main : Form
+    {
+        // CAN file path
+        private string logfile_path;
+
+        // Database file path
+        private string database_path;
+
+        //Status strip label
+        private ToolStripStatusLabel status_text = new ToolStripStatusLabel();
+
+        public CAN_Viewer_Main()
+        {
+            InitializeComponent();
+        }
+
+        private void CAN_Viewer_Main_Load(object sender, EventArgs e)
+        {
+            // Initialize status strip with "none" text
+            statusStrip_status.GripStyle = ToolStripGripStyle.Hidden;
+            statusStrip_status.Items.AddRange(new ToolStripItem[] { status_text });
+
+            statusStrip_status.LayoutStyle = ToolStripLayoutStyle.HorizontalStackWithOverflow;
+            statusStrip_status.ShowItemToolTips = true;
+            statusStrip_status.SizingGrip = false;
+            statusStrip_status.Stretch = false;
+            statusStrip_status.TabIndex = 0;
+
+            status_text.Size = new System.Drawing.Size(109, 17);
+            status_text.Text = "none";
+        }
+
+        private void openToolStripMenuItem_logfile_Click(object sender, EventArgs e)
+        {
+            // Open file dialog
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "txt files (*.txt)|*.txt";
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    // Get the path of specified file
+                    logfile_path = openFileDialog.FileName;
+
+                    // Update status bar with file name
+                    status_text.Text = Path.GetFileName(logfile_path);
+
+                    /* Opens file in notepad
+                    Process.Start("notepad.exe", logfile_path);
+                    */
+
+                    /* Read the contents of the file into a stream
+                    var fileStream = openFileDialog.OpenFile();
+
+                    using (StreamReader reader = new StreamReader(fileStream))
+                    {
+                        fileContent = reader.ReadToEnd();
+                    }
+                    */
+                }
+            }
+        }
+
+        private void openToolStripMenuItem_database_Click(object sender, EventArgs e)
+        {
+            // Open file dialog
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "txt files (*.txt)|*.txt";
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    // Get the path of specified file
+                    database_path = openFileDialog.FileName;
+
+                    // Update status bar with file name
+                    status_text.Text = Path.GetFileName(database_path);
+
+                    /* Opens file in notepad
+                    Process.Start("notepad.exe", database_path);
+                    */
+                    
+                    /* Read the contents of the file into a stream
+                    var fileStream = openFileDialog.OpenFile();
+
+                    using (StreamReader reader = new StreamReader(fileStream))
+                    {
+                        fileContent = reader.ReadToEnd();
+                    }
+                    */
+                }
+            }
+        }
+    }
+}
