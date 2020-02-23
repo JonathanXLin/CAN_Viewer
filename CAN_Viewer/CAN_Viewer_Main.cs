0702a2f3fab6849f95c60cf42d1de68de2846c68
Jonathan Lin <jlin1999@gmail.com>
2020-02-23 00:29:30 -0500

    logfile parser init

--- CAN_Viewer/CAN_Viewer_Main.cs
+++ CAN_Viewer/CAN_Viewer_Main.cs
@@ -15,10 +15,8 @@ namespace CAN_Viewer
 {
     public partial class CAN_Viewer_Main : Form
     {
-        // CAN file path
-        private string logfile_path;
-
         database_file_t database_file = new database_file_t();
+        logfile_t logfile = new logfile_t();
 
         //Status strip label
         private ToolStripStatusLabel status_text = new ToolStripStatusLabel();
@@ -28,6 +26,31 @@ namespace CAN_Viewer
             InitializeComponent();
         }
 
+        // Logfile, which contains points
+        public class logfile_t
+        {
+            public string path;
+
+            public List<logfile_point_t> point_list;
+            public int num_points;
+        }
+
+        // Direction enumeration for logfile point direction
+        enum Direction { Rx, Tx, unknown };
+
+        // Logfile point, corresponds to single line in logfile
+        public class logfile_point_t
+        {
+            public double timestamp;
+            public int channel;
+            public long id;
+            public int direction;
+            public int num_bytes;
+            public int[] data;
+            public int point_number;
+        }
+
+        // Database file, which contains message formats
         public class database_file_t
         {
             public string path;
@@ -35,6 +58,8 @@ namespace CAN_Viewer
             public List<message_format_t> message_list;
             public int num_messages;
         }
+
+        // Database file message format, which contains signal formats
         public class message_format_t
         {
             public string name;
@@ -49,6 +74,7 @@ namespace CAN_Viewer
         // Endian enumeration for DBC signal format
         enum Endian { little_endian, big_endian, unknown };
 
+        // Database file signal format
         public class signal_format_t
         {
             public string name;
@@ -74,10 +100,10 @@ namespace CAN_Viewer
             statusStrip_status.Stretch = false;
             statusStrip_status.TabIndex = 0;
 
-            logfile_path = "[no logfile selected]";
+            logfile.path = "[no logfile selected]";
 
             status_text.Size = new System.Drawing.Size(109, 17);
-            status_text.Text = logfile_path;
+            status_text.Text = logfile.path;
 
             // Initialize treeview with logfile and database root nodes
             TreeNode root_logfile = new TreeNode("CAN Logfiles");
@@ -97,11 +123,70 @@ namespace CAN_Viewer
 
                 if (openFileDialog.ShowDialog() == DialogResult.OK)
                 {
-                    // Get the path of specified file
-                    logfile_path = openFileDialog.FileName;
+                    // Populate logfile object
+                    logfile.path = openFileDialog.FileName;
+                    logfile.point_list = new List<logfile_point_t>();
+                    logfile.num_points = 0;
 
                     // Update status bar with file name
-                    status_text.Text = Path.GetFileName(logfile_path);
+                    status_text.Text = Path.GetFileName(logfile.path);
+
+                    // Add logfile to treeview
+                    TreeNode new_logfile_node = new TreeNode(Path.GetFileName(logfile.path));
+                    treeView_tree.Nodes[0].Nodes.Add(new_logfile_node);
+                    treeView_tree.Nodes[0].Expand();
+
+                    // Populates database_file object with database data
+                    using (StreamReader reader = new StreamReader(logfile.path))
+                    {
+                        // Continue reading if not at end
+                        while (!reader.EndOfStream)
+                        {
+                            // Read next line
+                            string curr_log_line = reader.ReadLine();
+                            string[] log_line_words = curr_log_line.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                            // Add and populate new logfile point
+                            logfile_point_t new_point = new logfile_point_t();
+                            new_point.timestamp = Double.Parse(log_line_words[0]);
+                            new_point.channel = Int32.Parse(log_line_words[1]);
+                            int index_of_x = log_line_words[2].IndexOf(@"x");
+
+                            if (index_of_x != -1) // Valid IDs end in x
+                                new_point.id = Int64.Parse(log_line_words[2].Remove(index_of_x, 1));
+                            else
+                                new_point.id = Int64.Parse(log_line_words[2]);
+
+                            if (Equals(log_line_words[3], "Rx"))
+                                new_point.direction = 0;
+                            else if (Equals(log_line_words[3], "Tx"))
+                                new_point.direction = 1;
+                            else
+                                new_point.direction = 2;
+
+                            new_point.num_bytes = int.Parse(log_line_words[4]);
+
+                            new_point.data = new int[new_point.num_bytes];
+
+                            for (int i = 0; i < new_point.num_bytes; i++)
+                                new_point.data[i] = int.Parse(log_line_words[5 + i]);
+
+                            new_point.point_number = int.Parse(log_line_words[5 + new_point.num_bytes]);
+
+                            logfile.point_list.Add(new_point);
+
+                            /*
+                            if (new_point.num_bytes == 0)
+                            {
+                                MessageBox.Show(curr_log_line + "\n\n" + new_point.timestamp + " " + new_point.channel + " " + new_point.id + " " + new_point.direction + " " + new_point.num_bytes + " " + new_point.point_number);
+                                foreach (int i in new_point.data)
+                                    MessageBox.Show(i.ToString());
+                            }
+                            */
+
+                            
+                        }
+                    }
 
                     /* Opens file in notepad
                     Process.Start("notepad.exe", logfile_path);
@@ -129,17 +214,15 @@ namespace CAN_Viewer
 
                 if (openFileDialog.ShowDialog() == DialogResult.OK)
                 {
-                    // Get the path of specified file
+                    // Populate database_file object
                     database_file.path = openFileDialog.FileName;
-
+                    database_file.message_list = new List<message_format_t>();
+                    database_file.num_messages = 0;
+                    
                     // Add database to treeview
                     TreeNode new_database_node = new TreeNode(Path.GetFileName(database_file.path));
                     treeView_tree.Nodes[1].Nodes.Add(new_database_node);
                     treeView_tree.Nodes[1].Expand();
-
-                    // Declare instance of message list and initializes length
-                    database_file.message_list = new List<message_format_t>();
-                    database_file.num_messages = 0;
 
                     /* Opens file in notepad */
                     //Process.Start("notepad.exe", database_file.path);
@@ -160,7 +243,7 @@ namespace CAN_Viewer
                                 // Parse message line and populate message_list with new item
                                 message_format_t new_message = new message_format_t();
                                 new_message.name = message_line_words[2];
-                                new_message.id = Int64.Parse(message_line_words[1]);
+                                new_message.id = Int64.Parse(message_line_words[1]) - (long)(8 * Math.Pow(16, 7)); // Last digit in hex is 8 too big for some reason
                                 new_message.length = Int32.Parse(message_line_words[3].Remove(0, 1));
                                 new_message.sender = message_line_words[4];
                                 database_file.message_list.Add(new_message);
@@ -218,16 +301,16 @@ namespace CAN_Viewer
                                             new_signal.unit = "none";
 
                                         if (Equals(signal_line_words[4].Substring(ampersand_index), "@1+"))
-                                            new_signal.endian = 1;
+                                            new_signal.endian = 0;
                                         else if (Equals(signal_line_words[4].Substring(ampersand_index), "@1-"))
-                                            new_signal.endian = 2;
+                                            new_signal.endian = 1;
                                         else
-                                            new_signal.endian = 3;
+                                            new_signal.endian = 2;
 
                                         database_file.message_list[database_file.num_messages - 1].signal_list.Add(new_signal);
                                         database_file.message_list[database_file.num_messages - 1].num_signals++;
 
-                                        //MessageBox.Show(curr_signal_line + "\t\n" + new_signal.name + " " + new_signal.start_bit + " " + new_signal.length + " " + new_signal.scale + " " + new_signal.offset + " " + new_signal.min + " " + new_signal.max + " " + new_signal.unit + " " + new_signal.endian);
+                                        //MessageBox.Show(curr_signal_line + "\n\n" + new_signal.name + " " + new_signal.start_bit + " " + new_signal.length + " " + new_signal.scale + " " + new_signal.offset + " " + new_signal.min + " " + new_signal.max + " " + new_signal.unit + " " + new_signal.endian);
 
                                     } while ((char)reader.Peek() == ' ');
                                 }
